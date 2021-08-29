9f3446e4c9e077659ea84fd011e1dbe96339d0fc
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2021-08-29 17:35:40 -0500

    Add project files.

---
+++ PrimeraClase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variables
+            // TipoDato NombreVariable ;
+            int edad=33;
+            string nombre = "Hugo";
+            double sueldo = 10000.0;
+            bool EsMayorEdad = true;
+            //DateTime FechaNacimiento = "05-04-1988";
+            //Char existe pero no lo vamos usar
+
+
+            Console.WriteLine("Mi nombre es " + nombre);
+            Console.WriteLine("Mi edad es " + edad);
+            Console.WriteLine("Mi sueldo es " + sueldo);
+            
+            //Pausa
+            Console.Read();
+
+
+
+
+
+
+        }
+    }
+}
